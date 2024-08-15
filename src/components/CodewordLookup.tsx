89d98207ef0fe944b2929a2bb1f8b3c1786b71d9
Taras Gordiienko <lozuvatka@gmail.com>
2024-08-15 09:36:03 +0100

    fix: fix the action id appearance

--- src/components/CodewordLookup.tsx
+++ src/components/CodewordLookup.tsx
@@ -9,7 +9,7 @@ const CodewordLookup: React.FC = () => {
   const handleLookup = async () => {
     try {
       const response = await axios.get(`http://localhost:3000/action/${codeword}`);
-      setActionId(response.data.id);
+      setActionId(response.data.action_id);
       setError(null);
     } catch (err) {
       setActionId(null);
